27cf52f37ae8b8792c1d40fd9db837fe7d1a7dc5
Bernardo <eubernardoassis@gmail.com>
2023-08-09 00:43:16 -0300

    front-end da home

---
+++ README.md
@@ -0,0 +1,2 @@
+Alguns alterações precisam ser feitas para o código funcionar na sua maquina
+*O link de algumas páginas por exemplo
--- index.php
+++ index.php
@@ -7,8 +7,23 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
+
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Blog pessoal</span>
+        </div>
+    </nav>
+    <br>
     <div class="container">
-        <h1>BLOG!</h1>
+        <div class="card mb-3">
+            <a href="/blog/news.php" style="text-decoration:none; color:currentColor;">
+                <div class="card-body">
+                    <h5 class="card-title" >Manchete</h5>
+                    <p class="card-text">Subtitulo</p>
+                    <p class="card-text"><small class="text-body-secondary">01/01/2000</small></p>
+                </div>
+        </div>
+            </a>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
